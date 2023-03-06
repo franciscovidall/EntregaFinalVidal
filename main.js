9524344c47d253d5b6f5db231b046e1a3490c49a
franciscovidal <fvidal1302@gmail.com>
2023-03-06 00:04:57 -0300

    primer commit

--- main.js
+++ main.js
@@ -9,18 +9,18 @@ class Vehiculo {
     }
 }
 
-const amarok = new Vehiculo(1, "VOLKSWAGEN", "AMAROK", 60000, "./img/amarok.jpg");
-const jeepRenegade = new Vehiculo(2, "JEEP", "RENEGADE", 70000, "img/jeeprenegade.jpg");
-const broncoSport = new Vehiculo(3, "FORD", "BRONCO SPORT", 53000, "img/broncosport.jpg");
-const jeepRubicon = new Vehiculo(4, "JEEP", "RUBICON", 62000, "img/jeeprubicon.jpg");
-const peugeot3008 = new Vehiculo(5, "PEUGEOT", "3008", 38000, "img/peugeot3008.jpg");
-const landRover = new Vehiculo(6, "LAND ROVER", "EVOQUE", 85000, "img/landrover.jpg");
+const amarok = new Vehiculo(1, "Volkswagen", "Amarok", 60000, "./img/amarok.jpg");
+const jeepRenegade = new Vehiculo(2, "Jeep", "Renegade", 70000, "img/jeeprenegade.jpg");
+const broncoSport = new Vehiculo(3, "Ford", "Bronco Sport", 53000, "img/broncosport.jpg");
+const jeepRubicon = new Vehiculo(4, "Jeep", "Rubicon", 62000, "img/jeeprubicon.jpg");
+const peugeot3008 = new Vehiculo(5, "Peugeot", "3008", 38000, "img/peugeot3008.jpg");
+const landRover = new Vehiculo(6, "Land Rover", "Evoque", 85000, "img/landrover.jpg");
 const bmwGs = new Vehiculo (7, "BMW", "GS 750", 27000, "img/bmwgs750.jpg");
-const reClassic = new Vehiculo(8, "ROYAL ENFIELD", "CLASSIC 350", 6500, "img/REClassic350.jpg");
-const reHimalayan = new Vehiculo(9, "ROYAL ENFIELD", "HIMALAYAN 400", 6800, "img/REHimalayan.jpg");
-const reInterceptor = new Vehiculo(10, "ROYAL ENFIELD", "INTERCEPTOR 650", 11200, "img/REInterceptor.jpg");
-const ram = new Vehiculo(11, "DODGE", "RAM", 98000, "img/ram.webp");
-const mercedes = new Vehiculo(12, "MERCEDES BENZ", "CLASE C250", 55000, "img/mercedes.jpg");
+const reClassic = new Vehiculo(8, "Royal Enfield", "Classic 350", 6500, "img/REClassic350.jpg");
+const reHimalayan = new Vehiculo(9, "Royal Enfield", "Himalayan 400", 6800, "img/REHimalayan.jpg");
+const reInterceptor = new Vehiculo(10, "Royal Enfield", "Interceptor 650", 11200, "img/REInterceptor.jpg");
+const ram = new Vehiculo(11, "Dodge", "RAM", 98000, "img/ram.webp");
+const mercedes = new Vehiculo(12, "Mercedes Benz", "Clase C250", 55000, "img/mercedes.jpg");
 
 //ARRAY CON TODOS LOS VEHICULOS
 
@@ -48,13 +48,13 @@ const mostrarVehiculos = () => {
                                 <h2> ${vehiculo.marca} </h2>
                                 <h2> ${vehiculo.modelo} </h2>
                                 <p> ${vehiculo.precio} </p>
-                                <button class = "btn colorBoton" id = "boton ${vehiculo.id}" > Comprar este vehículo </button>
+                                <button class = "btn colorBoton" id="boton ${vehiculo.id}" > Comprar este vehículo </button>
                             </div>
                         </div>
                         `
                         contenedorVehiculos.appendChild(card);
 
-        const boton = document.getElementById (`boton${vehiculo.id}`)
+        const boton = document.getElementById(`boton${vehiculo.id}`)
         card.addEventListener("click", () => {
             agregarAlCarrito(vehiculo.id);
         })
@@ -68,12 +68,14 @@ mostrarVehiculos();
 const agregarAlCarrito = (id) => {
     const vehiculoEnCarrito = carrito.find(vehiculo => vehiculo.id === id);
     if(vehiculoEnCarrito) {
-        vehiculoEnCarrito.cantidad ++;
+        vehiculoEnCarrito.cantidad++;
     } else {
         const vehiculo = vehiculos.find(vehiculo => vehiculo.id === id);
     carrito.push(vehiculo);
     }
-    console.log(carrito);
+    calcularTotal();
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
 const contenedorCarrito = document.getElementById("contenedorCarrito");
@@ -87,35 +89,63 @@ const mostrarCarrito = () => {
     contenedorCarrito.innerHTML = "";
     carrito.forEach(vehiculo => {
         const card = document.createElement("div");
-        card.classList.add("col-xl-3", "col-md-6", "col-sm-12");
+        card.classList.add("col-md-12");
         card.innerHTML = `
-                        <div class = "card">
-                            <img src = "${vehiculo.img}" class = "card-img-top imgVehiculos" alt = "${vehiculo.marca}">
+                        <div class ="card">
+                            <img src = "${vehiculo.img}" class = "card-img-top imgProductos" alt = "${vehiculo.marca}">
                             <div>
-                                <h2> ${vehiculo.marca} </h2>
-                                <h2> ${vehiculo.modelo} </h2>
+                                <h5> ${vehiculo.marca} </h5>
+                                <h5> ${vehiculo.modelo} </h5>
                                 <p> ${vehiculo.precio} </p>
-                                <p> ${vehiculo.cantidad} </p>
-                                <button class = "btn colorBoton" id = "eliminar ${vehiculo.id}" > Eliminar </button>
+                                <button class = "btn colorBoton" id="eliminar${vehiculo.id}" > Eliminar </button>
                             </div>
                         </div>
                         `
-                        contenedorCarrito.appendChild(card)
+        contenedorCarrito.appendChild(card);
 
-                        const boton = document.getElementById(`eliminar${vehiculo.id}`);
-                        boton.addEventListener("click",() => {
-                            eliminarDelCarrito(vehiculo.id);
-                        })
+        //Eliminar vehiculo uno por uno 
+        const boton = document.getElementById(`eliminar${vehiculo.id}`);
+        boton.addEventListener("click", () => {
+            eliminarDelCarrito(vehiculo.id);
+        })
     })
+    calcularTotal();
 }
 
-
-//Función para eliminar del carrito
 
 const eliminarDelCarrito = (id) => {
     const vehiculos = carrito.find(vehiculo => vehiculo.id === id);
     const indice = carrito.indexOf(vehiculos);
     carrito.splice(indice, 1);
     mostrarCarrito();
-    
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+//Total de la compra: 
+
+const total = document.getElementById("total");
+
+const calcularTotal = () => {
+    let totalCompra = 0; 
+    carrito.forEach(producto => {
+        totalCompra += producto.precio * producto.cantidad;
+    })
+    total.innerHTML = `Total: U$S${totalCompra}`;
+}
+
+//Eliminar todos los vehiculos del carrito: 
+
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+
+vaciarCarrito.addEventListener("click", () => {
+    eliminarTodoElCarrito();
+})
+
+const eliminarTodoElCarrito = () => {
+    carrito = []; 
+    mostrarCarrito();
+
+
+    localStorage.clear();
 }
